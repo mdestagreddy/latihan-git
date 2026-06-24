ad5291c609a0d0357ff508f3b308f64992ccfcfe
mdestagreddy <gsyv6645@gmail.com>
2026-06-24 14:42:56 +0800

    perubahan file

---
+++ array.js
@@ -0,0 +1,49 @@
+var nilaiArray = [2, 5, 1, 3, 4];
+var perubahanArray = [...nilaiArray];
+
+function hasilArray(nama, detail) {
+    if (JSON.stringify(nilaiArray) === JSON.stringify(perubahanArray) || detail != null) {
+        console.log(`[${nama}]${detail != null ? " " + detail : ""} ${JSON.stringify(nilaiArray)}`);
+    } else {
+        console.log(`[${nama}] Sebelum: ${JSON.stringify(perubahanArray)}`);
+        console.log(`[${nama}] Sesudah: ${JSON.stringify(nilaiArray)}`);
+    }
+    perubahanArray = typeof nilaiArray == "object" ? [...nilaiArray] : nilaiArray;
+}
+
+console.log(nilaiArray[0]);
+console.log(nilaiArray[2]);
+hasilArray("main");
+
+// length
+console.log(`Panjang array: ${nilaiArray.length}`);
+
+// push
+nilaiArray.push(7);
+hasilArray("push");
+
+// pop
+nilaiArray.pop();
+hasilArray("pop");
+
+// unshift
+nilaiArray.unshift(9);
+hasilArray("unshift");
+
+// shift
+nilaiArray.shift();
+hasilArray("shift");
+
+// join
+nilaiArray = ["Andra", "Taufik", "Desta"];
+hasilArray("join", "Sebelum:");
+nilaiArray = nilaiArray.join(" ");
+hasilArray("join", "Sesudah:");
+
+// split
+nilaiArray = "Muhammad Desta Greddy Aulia Rahman";
+hasilArray("split", "Sebelum:");
+nilaiArray = nilaiArray.split(" ");
+hasilArray("split", "Sesudah:");
+
+// sort
