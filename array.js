fd7e23a5afbb8c9e003fbcb1e9680bbfab0efa7c
mdestagreddy <gsyv6645@gmail.com>
2026-06-24 15:10:38 +0800

    perubahan file pada array.js

--- array.js
+++ array.js
@@ -1,22 +1,19 @@
 var nilaiArray = [2, 5, 1, 3, 4];
 var nilaiArraySebelumnya = [...nilaiArray];
 
-function hasilArray(nama, detail) {
+function hasilArray(nama, detail, tampilkanLength = true) {
     if (JSON.stringify(nilaiArray) === JSON.stringify(nilaiArraySebelumnya) || detail != null) {
-        console.log(`[${nama}]${detail != null ? " " + detail : ""} ${JSON.stringify(nilaiArray)}`);
+        console.log(`[${nama}]${detail != null ? " " + detail : ""}`, nilaiArray);
     } else {
-        console.log(`[${nama}] Sebelum: ${JSON.stringify(nilaiArraySebelumnya)}`);
-        console.log(`[${nama}] Sesudah: ${JSON.stringify(nilaiArray)}`);
+        console.log(`[${nama}] Sebelum:`, nilaiArraySebelumnya);
+        console.log(`[${nama}] Setelah:`, nilaiArray);
     }
     nilaiArraySebelumnya = typeof nilaiArray == "object" ? [...nilaiArray] : nilaiArray;
+    if (tampilkanLength) console.log(`Panjang/length:`, nilaiArray.length);
 }
 
-console.log(nilaiArray[0]);
-console.log(nilaiArray[2]);
-hasilArray("main");
-
 // length
-console.log(`Panjang array: ${nilaiArray.length}`);
+hasilArray("length");
 
 // push
 nilaiArray.push(7);
@@ -36,14 +33,28 @@ hasilArray("shift");
 
 // join
 nilaiArray = ["Andra", "Taufik", "Desta"];
-hasilArray("join", "Sebelum:");
+hasilArray("join", "Sebelum:", false);
 nilaiArray = nilaiArray.join(" ");
-hasilArray("join", "Sesudah:");
+hasilArray("join", "Setelah:");
 
 // split
 nilaiArray = "Muhammad Desta Greddy Aulia Rahman";
-hasilArray("split", "Sebelum:");
+hasilArray("split", "Sebelum:", false);
 nilaiArray = nilaiArray.split(" ");
-hasilArray("split", "Sesudah:");
+hasilArray("split", "Setelah:");
 
-// sort
+// sort/reverse
+nilaiArray = ["Andra", "Taufik", "Hendri", "Desta", "Dedi"];
+hasilArray("sort", "Sebelum:", false);
+nilaiArray = nilaiArray.sort();
+hasilArray("sort", "Sort:", false);
+nilaiArray = nilaiArray.reverse();
+hasilArray("sort", "Reverse:");
+
+// slice
+nilaiArray = nilaiArray.slice(1, 4);
+hasilArray("slice");
+
+// splice
+nilaiArray.splice(2, 0, "Heldi", "Dennis");
+hasilArray("splice");
