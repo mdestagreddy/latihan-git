a5c1781028a3d40bfa6bd0fe21083734eea5de0c
mdestagreddy <gsyv6645@gmail.com>
2026-06-24 10:56:29 +0800

    menambahkan dan mengubah file

---
+++ conditional.js
@@ -0,0 +1,84 @@
+/**if (true) {
+    console.log("Kode program dijalankan");
+}
+
+if (false) {
+  console.log("Kode program tidak dijalankan");
+
+
+var mood = "happy";
+if (mood == "bahagia") {
+    console.log("Saya sedang bahagia hari ini");
+}
+
+var angka = 1;
+if (angka == "1") {
+    console.log("Muncul Angka 1");
+}
+
+var mood = "sedih";
+if (mood == "happy") {
+    console.log("Saya sedang bahagia hari ini");
+} else if (mood == "angry") {
+    console.log("Saya sedang marah hari ini");
+} else {
+    console.log("Saya sedang tidak baik-baik saja");
+}
+
+var status = "open";
+var telur = "ready";
+var buah = "sold";
+if (status == "open") {
+    console.log("Saya akan membeli telur dan buah");
+    if (telur == "soldout" && buah == "soldout") {
+        console.log("Belanjaan saya tidak lengkap");
+    } else if (telur == "sold") {
+        console.log("Telur habis, Buah tersedia");
+    } else if (buah == "sold") {
+        console.log("Buah habis, Telur tersedia");
+    } else {
+        console.log("Telur dan buah tersedia");
+    }
+} else {
+    console.log("Minimarket tutup, saya pulang lagi");
+}
+
+var umur = 19;
+if (umur >= 17) {
+    console.log("Sudah memiliki KTP");
+    if (umur < 20) {
+        console.log("Umur kurang dari 20 tahun");
+    } else if (umur < 30) {
+        console.log("Usia antara 20-30 tahun");
+    } else if (umur < 40) {
+        console.log("Usia antara 30-40 tahun");
+    } else {
+        console.log("Sudah berumur");
+    }
+} else {
+    console.log("Belum memiliki KTP");
+}*/
+
+var warna = "pink";
+switch (warna) {
+    case "merah": {
+        console.log("Warna merah");
+        break;
+    }
+    case "biru": {
+        console.log("Warna biru");
+        break;
+    }
+    case "kuning": {
+        console.log("Warna kuning");
+        break;
+    }
+    case "hijau": {
+        console.log("Warna hijau");
+        break;
+    }
+    default: {
+        console.log("Warna yang dipilih tidak ada");
+        break;
+    }
+}
--- latihan.js
+++ latihan.js
@@ -1,4 +1,107 @@
 console.log("Halo semua");
 
-var umur = 19;
-console.log(umur + " tahun");
+var nama = "John Doe";
+var hari = "Selasa";
+var nomorUrut = 12;
+var quote = "Hari ini adalah hari";
+
+console.log(`Nama: ${nama}\nNomor Urut: ${nomorUrut}\n\n${quote} ${hari}.`);
+
+var open = false;
+if (open == true) {
+    console.log("BUKA");
+} else {
+    console.log("TUTUP");
+}
+
+// Operator Aritmatika
+console.log("=====================");
+console.log("Operasi Aritmatika");
+
+var a = 5;
+var b = 5;
+var tambah = a + b;
+var kurang = a - b;
+var kali = a * b;
+var bagi = a / b;
+var modulus = a % b;
+
+console.log(`hasil tambah: ${tambah}\nhasil kurang: ${kurang}\nhasil kali: ${kali}\nhasil bagi: ${bagi}\nhasil modulus: ${modulus}`);
+
+// Operator Perbandingan
+console.log("=====================");
+console.log("Operasi Perbandingan");
+
+console.log(1 == "1");
+console.log(1 === "1");
+console.log(1 >= 1);
+console.log(1 > 1);
+
+// Operator Kondisional (&& AND, || OR)
+console.log("=====================");
+console.log("Operator Kondisional");
+
+/**
+console.log(true || true);
+console.log(true || false);
+console.log(true && true);
+console.log(true && false);
+*/
+var status = "open";
+var jamBuka = 9;
+
+console.log(status == "open" && jamBuka >= 9);
+
+// JavaScript Strings
+console.log("=====================");
+console.log("JavaScript Strings");
+
+var word = "Web Programming";
+var word2 = " JavaScript";
+console.log(`${word}\nPanjang karakter: ${word.length}\nIndeks karakter ke-5: ${word.charAt(5)}`);
+console.log(word.concat(word2));
+console.log(`${word}${word2}, tahun 2026`);
+console.log(word.indexOf("Web"));
+console.log(word.indexOf("m"));
+console.log(word.indexOf("Node.js"));
+console.log(word.substring(4));
+console.log(word.toUpperCase());
+console.log(word.toLowerCase());
+
+var word3 = " Web Programming Node.js dan React ";
+console.log(word3.trim());
+console.log(word.replace("r", "R"));
+console.log(word.replaceAll(" ", "-"));
+
+// Mengubah menjadi string
+// .toString()
+// String(namaVariable)
+console.log("=====================");
+console.log("Mengubah menjadi string");
+
+var angka = 15;
+console.log(angka);
+console.log(angka+angka);
+angka = angka.toString();
+console.log(angka);
+console.log(angka+angka);
+
+// Mengubah menjadi Angka/Number
+// parseInt(namaVariable)
+// parseFloat(namaVariable)
+console.log("=====================");
+console.log("Mengubah menjadi Angka/Number");
+
+var kata = "7";
+console.log(kata);
+console.log(Number(kata));
+console.log(parseInt(kata));
+
+var angka1 = 1.5;
+var angka2 = 2.7;
+
+console.log(angka1);
+console.log(angka2);
+
+var angka3 = angka1 + angka2;
+console.log(angka3);
