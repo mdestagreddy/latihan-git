0acb7a86f56d8c02da34d43439b38e9aae978e2c
mdestagreddy <gsyv6645@gmail.com>
2026-06-30 14:26:12 +0800

    update

--- latihan/db.sql
+++ latihan/db.sql
@@ -8,10 +8,13 @@ create table mahasiswa{
 };
 
 INSERT INTO mahasiswa(nim, nama, prodi, jenisKelamin, alamat, nohp)
-VALUES (12345, "M. Desta Geddy", "TIK", "Laki-laki", "Samarinda", "081234567890");
+VALUES (12345, "M. Desta Greddy", "TIK", "Laki-laki", "Samarinda", "081234567890");
 
 UPDATE mahasiswa
 SET alamat="Samarinda - TikTok"
 WHERE nim=12345;
 
 DELETE FROM mahasiswa WHERE nim=12345;
+
+INSERT INTO mahasiswa(nim, nama, prodi, jenisKelamin, alamat, nohp)
+VALUES (54321, "realme", "TIK", "Laki-laki", "Samarinda", "081122334455");
